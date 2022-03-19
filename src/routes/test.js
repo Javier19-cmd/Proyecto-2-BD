ef4f71504eacbc09e30d40831f423196d18ba7fc
Hansel López <halm29799@gmail.com>
2022-03-19 15:12:25 -0600

    Fix insert test

--- src/routes/test.js
+++ src/routes/test.js
@@ -2,20 +2,6 @@ const { Router } = require('express');
 const router = Router();
 const pool = require("./db");
 
-<<<<<<< Updated upstream
-router.post("/addTest", (req, res) => {
-  const { name, description, duration, price, category, image } = req.body;
-  pool.query(
-    "INSERT INTO test (name, description, duration, price, category, image) VALUES ($1, $2, $3, $4, $5, $6)",
-    [name, description, duration, price, category, image],
-    (error, results) => {
-      if (error) {
-        throw error;
-      }
-      res.status(200).send(`Test added with ID: ${results.insertId}`);
-    }
-  );
-=======
 router.post("/addTest", async (req, res) => {
   try {
     const { test } = req.body;
@@ -28,7 +14,6 @@ router.post("/addTest", async (req, res) => {
     console.error(`-------------ERROR-------------\n${err.message}`);
     res.status(200).send({ code: 0, error: err.message });
   }
->>>>>>> Stashed changes
 })
 
 module.exports = router;
