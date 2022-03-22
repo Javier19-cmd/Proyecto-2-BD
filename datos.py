e1aaa2cffbde34c66772ed12302932dd72b4cee6
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-03-21 20:49:10 -0600

    archivo con los datos de la base de datos

---
+++ datos.py
@@ -0,0 +1,19 @@
+#Host de la base de datos.
+def host():
+    return 'project2db-do-user-11101874-0.b.db.ondigitalocean.com'
+
+#Usuario de la base de datos.
+def user():
+    return 'doadmin'
+
+#Contraseña de la base de datos.
+def passw():
+    return 'aYSwBxxQYMU9ZQmJ'
+
+#Nombre de la base de datos.
+def BD():
+    return 'dbproject2'
+
+#Puerto de la base de datos.
+def port(): 
+    return '25060'
