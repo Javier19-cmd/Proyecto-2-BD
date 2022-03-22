b17da98c3dba4be5d47e00c25004e6795a10a24b
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-03-22 14:22:11 -0600

    agregando cosas

--- registro.py
+++ registro.py
@@ -1,3 +1,16 @@
+import psycopg2 #Librería para la base de datos.
+from datos import * #Jalando el archivo que tiene los datos de la BD.
+
+def abrir_BD(): #Método que servirá para poder ingresar los datos del usuario en la base de datos.
+    #Conexión a la base de datos.
+    conexion1 = psycopg2.connect(
+            host=host(), #Host de la base de datos.
+            user= user(), #Usuario de la base de datos.
+            password=passw(), #Contraseña de la base de datos.
+            database=BD(), #Base de datos que se usará.
+            port=port() #Puerto de la base de datos.
+    )
+
 #Archivo que se encarga de registrar a las personas en la base de datos.
 def registro():
     #Menú para registrar a las personas en la base de datos.
@@ -12,6 +25,6 @@ def registro():
     contraseña = input("Ingrese su contraseña: ")
 
     correo = input("Ingrese su correo: ")
-    
+
     #Plan básico, estándar y avanzado.
     plan = input("Ingrese su plan: ")
