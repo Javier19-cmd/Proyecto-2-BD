ce722815b54be85cc1ffdb9de66b56b246836d77
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-03-30 17:46:19 -0600

    sabiendo algunas contraseñas

--- ra.py
+++ ra.py
@@ -1,10 +1,21 @@
 import random
 import cryptocode
 
-#Encriptando contraseña.
+#Desencriptar las contraseñas.
 passkey = 'UVG' #Llave para encriptar.
 
+#Usuario de Roberto.
 contraseña = "Ctkh+dE=*L9B8Q6RaprHA67FEfruFCA==*aOfAN/I9Zcrqwac/M+tdiA==*j8InvuoXCnm7gOvsQ0/niQ=="
+
+conn = cryptocode.decrypt(contraseña, passkey) #Contraseña encriptada.
+
+print(conn)
+
+#Usuario de Juan (cuenta comercial)
+passkey = 'UVG' #Llave para encriptar.
+
+#Usuario de Roberto.
+contraseña = "1FVCfbk=*UZw4MqzQv8RdEPpZFK+UJg==*KALuAlWCwEeiaVOzTBTeqg==*tUukpaUeKavsN9C+9toDqg=="
 
 conn = cryptocode.decrypt(contraseña, passkey) #Contraseña encriptada.
 
