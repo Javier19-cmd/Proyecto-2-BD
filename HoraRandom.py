eb51ffb95216fc37e1baa4762f74c399af6ddb5d
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-22 14:38:04 -0600

    arreglando cosas

--- HoraRandom.py
+++ HoraRandom.py
@@ -3,13 +3,16 @@ from random import randrange
 import datetime 
 import random
 
+#Función generadora aleatoria de horas.
 def random_date(start,l): #Función que genera las horas aleatorias.
-   current = start
+   current = start #Fecha con la que se va a empezar a generar aleatoriamente las horas.
    while l >= 0:
-      curr = current + datetime.timedelta(minutes=randrange(60))
-      yield curr
-      l-=1
+      curr = current + datetime.timedelta(minutes=randrange(60)) #Generando una hora aleatoria
+      yield curr #Retornando cada una de las horas generadas.
+      l-=1 #Diferencia entre minutos.
 
+"""
+#Sección para pedir datos y generar números aleatorios.
 hora = random.randint(1, 24) #Generando hora random.
 mins = random.randint(0,60)  #Generando minutos random.
 año = int(input("Ingrese el año "))
@@ -19,4 +22,6 @@ dia = int(input("Ingrese el día "))
 startDate = datetime.datetime(año,mes,dia,hora,mins) #Variable que guarda la fecha y la hora.
 
 for x in random_date(startDate,8000): #For que genera las horas.
-  print(x.strftime("%d/%m/%y %H:%M:%S"))
+  z = x.strftime("%Y-%m-%d %H:%M:%S")
+  print(z)
+"""
--- simulacion.py
+++ simulacion.py
@@ -3,7 +3,9 @@ Por parte del programa se tiene que solicitar la fecha y la cantidad de visualiz
 """
 from datos import * #Importando los datos de la BD.
 import psycopg2     #Importando la librería de la BD.
-import random 
+import random
+import datetime
+from HoraRandom import *
 
 def generador():
 
@@ -19,29 +21,68 @@ def generador():
     cursor1 = conexion1.cursor() #Cursor de la conexión.
 
     #Pedirle al usuario la fecha y la cantidad de visualizaciones que se desean generar.
-    fecha = input("Ingrese la fecha en la que desea hacer la simulación (esta deber ir como MM-DD-YYYY) ")
+    año = int(input("Ingrese el año en la que desea hacer la simulación "))
+    mes = int(input("Ingrese el mes en el que desea hacer la simulación "))
+    dia = int(input("Ingrese el día en el que desea hacer la simulación "))
     cantidad = int(input("ingrese la cantidad de visualizaciones que desea generar en el sistema "))
+    
+    #Horas aleatorias.
+    hora = random.randint(1,24)
+    mins = random.randint(0, 60)
+    startDate = datetime.datetime(año, mes, dia, hora, mins)
 
     #Print de verificación.
-    print(fecha)
+    print(startDate)
     print(cantidad)
 
-    sql = "INSERT INTO prueba VALUES(%s)"
+    sql = "INSERT INTO prueba VALUES(%s, %s, %s, %s, %s, %s)"
 
-    #Selección random de una película.
-    sql1 = "SELECT RANDOM() AS orden, id, nombre, link FROM videos ORDER BY orden limit 1"
-    
-    cursor1.execute(sql1)
-
-    a = cursor1.fetchall()
-
-    print(a)
-
-    #Haciendo loop de prueba.
+    #Haciendo loop de prueba. Este loop se corre en base a la cantidad de 
+    #vistas que se quieren generar.
     for i in range(cantidad): 
-        print(fecha)
+        #print(fecha)
 
-        cursor1.execute(sql, (fecha,))
+        #Selección random de una película.
+        sql1 = "SELECT RANDOM() AS orden, id, nombre, link FROM videos ORDER BY orden limit 1"
+        
+        cursor1.execute(sql1) #Corriendo el sql.
+
+        a = cursor1.fetchall()
+
+        print(a) #Trayendo la tupla de las películas. Esto es una lista de dos dimensiones.
+
+        listaP = [a[0][1], a[0][2], a[0][3]] #Lista de las películas aleatorias a ver.
+
+        print("Lista", listaP[0], listaP[1], listaP[2]) #Aquí se está imprimiendo la película aleatoria con su id, nombre y link.
+
+        #print(a[0][1])
+
+        id = listaP[0] #Obteniendo el id de la película.
+        nombre = listaP[1] #Obteniendo el nombre de la película.
+        link = listaP[2] #Obteniendo el link de la película.
+
+        #Seleccionando perfil aleatorio de prueba.
+        sql2 = "SELECT RANDOM() AS orden, perfil FROM perfiles ORDER BY orden limit 1"
+
+        cursor1.execute(sql2) #Corriendo el sql.
+        
+        b = cursor1.fetchall()
+
+        print(b) #Imprimiendo la tupla que trae al usuario. Es un listado de dos dimensiones. 
+
+        listaU = [b[0][1]] #Se guarda en la listaU solamente el nombre del perfil que se quiere meter a la BD.
+        
+        print(listaU[0]) #Imprimiendo al perfil que verá aleatoriamente la película.
+
+        perfil = listaU[0] #Obteniendo el perfil que verá las películas.
+
+        visto = 1 #Variable para el visto.
+
+        #Dando formato a las fechas y a las horas
+        for x in random_date(startDate, cantidad):
+            fecha1 = x.strftime("%Y-%m-%d %H:%M:%S")
+
+        cursor1.execute(sql, (perfil, nombre, link, visto, fecha1, id,))
 
     conexion1.commit()
 
