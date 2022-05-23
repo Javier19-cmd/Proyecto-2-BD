9a62497b514ba0d57f0666fb316bdca65c7b220f
Javier Valle <63107636+Javier19-cmd@users.noreply.github.com>
2022-05-23 14:26:41 -0600

    cambios

--- simulacion.py
+++ simulacion.py
@@ -16,11 +16,11 @@ def generador():
 
     #Conexión a la base de datos.
     conexion1 = psycopg2.connect(
-            host=host(), #Host de la base de datos.
-            user= user(), #Usuario de la base de datos.
-            password=passw(), #Contraseña de la base de datos.
-            database=BD(), #Base de datos que se usará.
-            port=port() #Puerto de la base de datos.
+        host=host(), #Host de la base de datos.
+        user= user(), #Usuario de la base de datos.
+        password=passw(), #Contraseña de la base de datos.
+        database=BD(), #Base de datos que se usará.
+        port=port() #Puerto de la base de datos.
     )
 
     cursor1 = conexion1.cursor() #Cursor de la conexión.
@@ -52,7 +52,7 @@ def generador():
         
         cursor1.execute(sql1) #Corriendo el sql.
 
-        a = cursor1.fetchall()
+        a = cursor1.fetchall() #Guardando los datos obtenidos.
 
         print(a) #Trayendo la tupla de las películas. Esto es una lista de dos dimensiones.
 
